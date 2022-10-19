0376069984b0974a2061d15e53f1d5fbebb9b97f
dextermd <dextermdpay@gmail.com>
2022-10-19 21:54:44 +0300

    19.10_2

---
+++ Dextermd.ITDeveloper.Mvc/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dextermd.ITDeveloper.Mvc.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult List()
+        {
+            return View();
+        }
+    }
+}
--- Dextermd.ITDeveloper.Mvc/Program.cs
+++ Dextermd.ITDeveloper.Mvc/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Routing.Template;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -19,6 +21,14 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "productlist",
+    pattern: "{controller=Product}/{action=List}/{id?}");
+
+app.MapControllerRoute(
+    name: "alternative",
+    pattern: "{controller=Product}/{action=Index}/{id?}");
 
 app.MapControllerRoute(
     name: "default",
