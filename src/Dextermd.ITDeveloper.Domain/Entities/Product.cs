b2a9213ec653a3c17812f3392a66a3173647be73
DEXTER MD <dextermdpay@gmail.com>
2022-10-21 17:44:54 +0300

    Product Start

---
+++ src/Dextermd.ITDeveloper.Domain/Entities/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dextermd.ITDeveloper.Domain.Entities
+{
+    public class Product
+    {
+        public Product()
+        {
+
+        }
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public decimal Amount { get; set; }
+        public int Stock { get; set; }
+    }
+}
